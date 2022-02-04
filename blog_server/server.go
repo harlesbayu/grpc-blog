27721be793745af7cad8079da7ac25a2be7b23e6
Harles Bayu Anggara <harles>
2022-02-04 15:33:41 +0700

    skeleton service

---
+++ blog_server/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"github.com/harlesbayu/grpc-blog/blogpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+	// if we crash the go code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	fmt.Println("Blog Service Started")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:5051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{}
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch)
+
+	// Block until a signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the server")
+	lis.Close()
+	fmt.Println("End of program")
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/harlesbayu/grpc-blog
+
+go 1.16
+
+require (
+	go.mongodb.org/mongo-driver v1.8.3 // indirect
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.25.0
+)
